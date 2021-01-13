c14ead44884f8197aabff2d22746f52e6b5528fb
Selino Valdes <selinov@gmail.com>
2021-01-13 14:12:02 -0800

    Resetting after icloud problem.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import { createStore, applyMiddleware, compose } from "redux"
 import thunk from "redux-thunk"
 import albumsReducer from "./reducers/albumsReducer"
 import "./App.css"
-// import ListState from "./components/ListState"
-import ListRedux from "./components/ListRedux"
+import ListState from "./components/ListState"
+import List from "./components/ListState"
+// import ListRedux from "./components/ListRedux"
+import Gif from "./components/Gif"
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(
@@ -17,7 +19,7 @@ function App() {
   return (
     <Provider store={store}>
       <div className='App'>
-        <ListRedux />
+        <Gif />
       </div>
     </Provider>
   )
--- src/components/ListState.js
+++ src/components/ListState.js
@@ -1,21 +1,26 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
+// import AlbumForm from "./AlbumForm"
+import { withFormik, Form, Field, Formik } from "formik"
+import * as Yup from "yup"
 
-function List() {
+export default function List() {
   const [state, setState] = useState({
     albums: [],
   })
 
-  useEffect(() => {
-    getAlbumList()
-  }, [])
+  // useEffect(() => {
+  //   getAlbumList("https://jsonplaceholder.typicode.com/albums")
+  // }, [])
 
-  const getAlbumList = () =>
-    fetch("https://jsonplaceholder.typicode.com/albums")
-      .then((res) => res.json())
-      .then((data) => setState({ albums: data.slice(0, 5) }))
-      .catch((err) => {
-        console.log(err)
-      })
+  async function fetchData(url) {
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      setState({ albums: data.slice(0, 5) })
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   const albumItems = state.albums.map((album) => (
     <li key={album.id}>
@@ -23,12 +28,46 @@ function List() {
     </li>
   ))
 
+  const albumListForm = ({ values, errors, touched }) => {
+    return (
+      <Form>
+        <div>
+          {touched.url && errors.url && <p>{errors.url}</p>}
+          <Field type='text' name='url' placeholder='url' />
+        </div>
+        <div>
+          {touched.butt && errors.butt && <p>{errors.butt}</p>}
+          <Field type='text' name='butt' placeholder='butt' />
+        </div>
+        <input type='submit' value='Submit' />
+      </Form>
+    )
+  }
+
+  const AlbumForm = (props) => {
+    const FormWithFormik = withFormik({
+      mapPropsToValues({ url, butt }) {
+        return {
+          url: url || "",
+          butt: butt || "",
+        }
+      },
+      validationSchema: Yup.object().shape({
+        url: Yup.string().url().required("Url is required."),
+        butt: Yup.string().email().required("Butt is required."),
+      }),
+      handleSubmit(values) {
+        fetchData(values.url)
+      },
+    })(albumListForm)
+    return <FormWithFormik />
+  }
+
   return (
     <div>
-      <h1>Albums</h1>
+      <h1>Album List</h1>
       <ul>{albumItems}</ul>
+      <AlbumForm baseUrl='' />
     </div>
   )
 }
-
-export default List
